da978ece2761c4b2a2a7ee513c52cb0d53ddd265
Unknown <shobu13@hotmail.fr>
2018-12-21 09:39:24 +0100

    ajout yet another swagger generator

--- theNightmareWeShare/settings.py
+++ theNightmareWeShare/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'markdownx',
     'nested_admin',
     'rest_framework',
-    'rest_framework_swagger',
+    'drf_yasg',
     'rest_framework_jwt',
 
     'core',
--- theNightmareWeShare/urls.py
+++ theNightmareWeShare/urls.py
@@ -13,17 +13,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework.settings import api_settings
 
 from theNightmareWeShare import settings
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TheNightmareWeShare API",
+        default_version='BETA',
+        description="enpoints de l'application the nightmare we share",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    validators=['flex', 'ssv'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('', get_swagger_view(title='theNightmareWeShare')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
     path('nested_admin/', include('nested_admin.urls')),
